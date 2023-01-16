ed441255c47d19dc5bc75208c4ee288e9d0c37ae
saikatterishetty <rao.saikrishna@gmail.com>
2023-01-15 22:14:07 -0600

    Added Linked List Impl

---
+++ src/main/java/org/saikrishna/rao/learning/dsalgo/linkedlist/SKLinkedList.java
@@ -0,0 +1,91 @@
+package org.saikrishna.rao.learning.dsalgo.linkedlist;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+public class SKLinkedList<T extends Comparable<T>> {
+
+    private Optional<Node<T>> headNode;
+
+    private Integer count;
+
+    public SKLinkedList() {
+        headNode = Optional.empty();
+        count = 0;
+    }
+
+    public void push(T data) {
+        if (headNode.isEmpty()) {
+            headNode = Optional.of(new Node<>(data));
+        } else {
+            Node<T> tailNode = tailNode().get();
+            tailNode.setNextNode(Optional.of(new Node<>(data)));
+        }
+        count = count + 1;
+    }
+
+    public T newHead(T data) {
+        Optional<Node<T>> old = this.headNode;
+        Node<T> newHead = new Node<>(data);
+        newHead.setNextNode(old);
+        this.headNode = Optional.of(newHead);
+        return this.headNode.get().getData();
+    }
+
+    public Node<T> getHead() {
+        return this.headNode.isPresent() ? this.headNode.get() : null;
+    }
+
+    public Node<T> pop() {
+        if(this.headNode.isEmpty()) return null;
+        Optional<Node<T>> tailNode = tailNode();
+        if (tailNode.isPresent()) {
+            Node<T> lastNode = tailNode.get();
+            Optional<Node<T>> tNode = previousNodeTo(lastNode);
+            if(tNode.isPresent()){
+                tNode.get().setNextNode(null);
+                this.count = this.count - 1;
+                return lastNode;
+            }
+        }
+        return null;
+    }
+
+    public Optional<Node<T>> previousNodeTo(Node<T> aNode) {
+        Node<T> node;
+
+        for (node = this.headNode.get(); node.getNextNode().isPresent() && node.getNextNode().equals(aNode); node = node.getNextNode().get()) {
+        }
+
+        return Optional.ofNullable(node);
+    }
+
+
+    public Integer count() {
+        return count;
+    }
+
+    protected Optional<Node<T>> tailNode() {
+        if (this.headNode.isEmpty()) {
+            return this.headNode;
+        }
+
+        Node<T> node;
+
+        for (node = this.headNode.get(); node.getNextNode().isPresent(); node = node.getNextNode().get()) {
+        }
+
+        return Optional.of(node);
+    }
+
+
+    @Data
+    @RequiredArgsConstructor
+    public static class Node<T> {
+        private final T data;
+        private Optional<Node<T>> nextNode = Optional.empty();
+
+    }
+}
---
+++ src/test/java/org/saikrishna/rao/learning/dsalgo/linkedlist/SKLinkedListTest.java
@@ -0,0 +1,52 @@
+package org.saikrishna.rao.learning.dsalgo.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SKLinkedListTest {
+
+
+    @Test
+    public void testAddItem() {
+        SKLinkedList<Integer> skLinkedList = new SKLinkedList<>();
+        skLinkedList.push(10);
+        skLinkedList.push(20);
+        skLinkedList.push(30);
+        skLinkedList.push(40);
+        assertThat(skLinkedList.count()).isEqualTo(4);
+    }
+
+    @Test
+    void testGetLast() {
+        SKLinkedList<Integer> skLinkedList = new SKLinkedList<>();
+        skLinkedList.push(10);
+        skLinkedList.push(20);
+        assertThat(skLinkedList.tailNode().get().getData()).isEqualTo(20);
+    }
+
+    @Test
+    void testNewHead() {
+        SKLinkedList<Integer> skLinkedList = new SKLinkedList<>();
+        skLinkedList.push(10);
+        skLinkedList.newHead(20);
+        assertThat(skLinkedList.getHead().getData()).isEqualTo(20);
+    }
+
+    @Test
+    void testPop() {
+        SKLinkedList<Integer> skLinkedList = new SKLinkedList<>();
+        skLinkedList.push(10);
+        skLinkedList.push(20);
+        skLinkedList.push(30);
+        assertThat(skLinkedList.pop().getData()).isEqualTo(30);
+        assertThat(skLinkedList.count()).isEqualTo(2);
+    }
+
+    @Test
+    void testPopNull() {
+        SKLinkedList<Integer> skLinkedList = new SKLinkedList<>();
+        assertThat(skLinkedList.pop()).isNull();
+    }
+
+}
